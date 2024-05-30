b0d1238f9052b53e1c7f7ead68253ff69144dbf0
apa247 <david.aparicio247@hotmail.com>
2024-05-30 23:36:36 +0200

    aa

--- src/main.c
+++ src/main.c
@@ -6,114 +6,117 @@
 /*   By: daparici <daparici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 00:53:00 by Ardeiro           #+#    #+#             */
-/*   Updated: 2024/05/28 03:14:44 by daparici         ###   ########.fr       */
+/*   Updated: 2024/05/30 23:32:48 by daparici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube.h"
 
-void    ft_init_game(t_data *data)
+void	ft_init_game(t_data *data)
 {
-    data->game = ft_calloc(sizeof(t_game), 1);
-    if (!data->game)
-        ft_exit(data, "error in memory");
-    data->game->mlx = NULL;
-    data->game->mlx_window = NULL;
-    data->game->win_size_y = 10;
-    data->game->win_size_x = 0;
+	data->game = ft_calloc(sizeof(t_game), 1);
+	if (!data->game)
+		ft_exit(data, "error in memory");
+	data->game->mlx = NULL;
+	data->game->mlx_window = NULL;
+	data->game->win_size_y = 10;
+	data->game->win_size_x = 0;
 }
 
-void    ft_init_data(t_data *data)
+void	ft_init_data(t_data *data)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    data->map_width = -1;
-    data->map_height = -1;
-    data->floor = NULL;
-    data->ceiling = NULL;
-    data->north_path = NULL;
-    data->south_path = NULL;
-    data->east_path = NULL;
-    data->west_path = NULL;
-    data->map = ft_calloc(sizeof(char *),  100);
-    if (!data->map)
-        ft_exit(data, "Error: Malloc failed!!\n");
-    while (i < 100)
-    {
-        data->map[i] = ft_calloc(sizeof(char), 100);
-        if (!data->map[i])
-            ft_exit(data, "Error: Malloc failed!!\n");
-        i++;
-    }
-    return ;
+	i = 0;
+	data->map_width = -1;
+	data->map_height = -1;
+	data->floor = NULL;
+	data->ceiling = NULL;
+	data->north_path = NULL;
+	data->south_path = NULL;
+	data->east_path = NULL;
+	data->west_path = NULL;
+	data->map = ft_calloc(sizeof(char *), 100);
+	if (!data->map)
+		ft_exit(data, "Error: Malloc failed!!\n");
+	while (i < 100)
+	{
+		data->map[i] = ft_calloc(sizeof(char), 100);
+		if (!data->map[i])
+			ft_exit(data, "Error: Malloc failed!!\n");
+		i++;
+	}
+	return ;
 }
 
 
 
-static int  ft_check_args(const int argc, const char **argv)
+static int	ft_check_args(const int argc, const char **argv)
 {
-    int     i;
-    int     ext_len;
-    char    *ext;
+	int		i;
+	int		ext_len;
+	char	*ext;
 
-    ext = ".cub";
-    if (argc != 2)
-    {
-        perror("Error: Invalid number of arguments!!\n");
-        return (EXIT_FAILURE);
-    }
-    else
-    {
-        ext_len = ft_strlen(argv[1]);
-        i = ext_len - 4;
-        while (i < ext_len)
-        {
-            if (argv[1][i] != ext[i - ext_len + 4])
-            {
-                perror("Error: Wrong file extension!!\n");
-                return (EXIT_FAILURE);
-            }
-            i++;
-        }
-    }
-    return (EXIT_SUCCESS);
+	ext = ".cub";
+	if (argc != 2)
+	{
+		perror("Error: Invalid number of arguments!!\n");
+		return (EXIT_FAILURE);
+	}
+	else
+	{
+		ext_len = ft_strlen(argv[1]);
+		i = ext_len - 4;
+		while (i < ext_len)
+		{
+			if (argv[1][i] != ext[i - ext_len + 4])
+			{
+				perror("Error: Wrong file extension!!\n");
+				return (EXIT_FAILURE);
+			}
+			i++;
+		}
+	}
+	return (EXIT_SUCCESS);
 }
 
-void closewin(t_data *data)
+int	closewin(t_data *data)
 {
-    ft_exit(data, "Finish Game");
+	ft_exit(data, "Finish Game");
+	return (0);
 }
-void    start_game(t_data *data)
-{
-    t_game  *aux;
 
-    aux = data->game;
-    aux->mlx = mlx_init();
-    if (!aux->mlx)
-        ft_exit(data, "Error in mlx_init");
-    if (mlx_get_screen_size(aux->mlx, &aux->win_size_x, &aux->win_size_y))
-        ft_exit(data, "Error in mlx_get_screen_size");
-    aux->mlx_window = mlx_new_window(aux->mlx, aux->win_size_x, aux->win_size_y, "Cub3d");
-    if (!aux->mlx_window)
-        ft_exit(data, "Error in mlx_window");
-    
-    mlx_loop(aux->mlx);
+void	start_game(t_data *data)
+{
+	t_game	*aux;
+
+	aux = data->game;
+	aux->mlx = mlx_init();
+	if (!aux->mlx)
+		ft_exit(data, "Error in mlx_init");
+	if (mlx_get_screen_size(aux->mlx, &aux->win_size_x, &aux->win_size_y))
+		ft_exit(data, "Error in mlx_get_screen_size");
+	aux->mlx_window = mlx_new_window(aux->mlx, aux->win_size_x, \
+			aux->win_size_y, "Cub3d");
+	if (!aux->mlx_window)
+		ft_exit(data, "Error in mlx_window");
+	mlx_hook(aux->mlx_window, 17, 1L << 17, closewin, data);
+	mlx_loop(aux->mlx);
 }
 
 int	main(int argc, char **argv)
 {
-    t_data  *data;
+	t_data	*data;
 
-    data = ft_calloc(sizeof(t_data), 1);
-    if (!data)
-        return (perror("error in malloc"), free(data), EXIT_FAILURE);
-    ft_init_data(data);
-    ft_init_game(data);
-    if (ft_check_args(argc, (const char **)argv))
-        return (ft_free_mem(data), free(data->game), free(data), EXIT_FAILURE);
-    if (ft_parsing(data, argv[1]))
-        return (ft_free_mem(data), free(data->game), free(data), EXIT_FAILURE);
-    start_game(data);
-    return (ft_free_mem(data), free(data), EXIT_SUCCESS);
+	data = ft_calloc(sizeof(t_data), 1);
+	if (!data)
+		return (perror("error in malloc"), free(data), EXIT_FAILURE);
+	ft_init_data(data);
+	ft_init_game(data);
+	if (ft_check_args(argc, (const char **)argv))
+		return (ft_free_mem(data), free(data->game), free(data), EXIT_FAILURE);
+	if (ft_parsing(data, argv[1]))
+		return (ft_free_mem(data), free(data->game), free(data), EXIT_FAILURE);
+	start_game(data);
+	return (ft_free_mem(data), free(data), EXIT_SUCCESS);
 }
--- src/parsing/colors.c
+++ src/parsing/colors.c
@@ -3,112 +3,112 @@
 /*                                                        :::      ::::::::   */
 /*   colors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Ardeiro <Ardeiro@student.42.fr>            +#+  +:+       +#+        */
+/*   By: daparici <daparici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 02:03:45 by Ardeiro           #+#    #+#             */
-/*   Updated: 2024/05/19 08:36:31 by Ardeiro          ###   ########.fr       */
+/*   Updated: 2024/05/30 21:36:24 by daparici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube.h"
 
-static char *ft_read_number(const char *str, int *i)
+static char	*ft_read_number(const char *str, int *i)
 {
-    int     j;
-    char    *number;
+	int		j;
+	char	*number;
 
-    j = 0;
-    number = ft_calloc(sizeof(char), (3 + 1));
-    while (ft_is_space(str[*i]))
-        (*i)++;
-    while (str[*i] != ',' && !ft_is_space(str[*i]) && j < 3)
-    {
-        number[j] = str[*i];
-        j++;
-        (*i)++;
-    }
-    return (number);
+	j = 0;
+	number = ft_calloc(sizeof(char), (3 + 1));
+	while (ft_is_space(str[*i]))
+		(*i)++;
+	while (str[*i] != ',' && !ft_is_space(str[*i]) && j < 3)
+	{
+		number[j] = str[*i];
+		j++;
+		(*i)++;
+	}
+	return (number);
 }
 
-static void ft_check_comma(t_data *data, const char *str, int *i)
+static void	ft_check_comma(t_data *data, const char *str, int *i)
 {
-    while (ft_is_space(str[*i]))
-        (*i)++;
-    if (str[*i] != ',')
-        ft_exit(data, "Error: RGB format not valid!!\n");
-    (*i)++;
-    return ;
+	while (ft_is_space(str[*i]))
+		(*i)++;
+	if (str[*i] != ',')
+		ft_exit(data, "Error: RGB format not valid!!\n");
+	(*i)++;
+	return ;
 }
 
-static void ft_check_end(t_data *data, const char *str, int *i)
+static void	ft_check_end(t_data *data, const char *str, int *i)
 {
-    while (ft_is_space(str[*i]))
-        (*i)++;
-    if (str[*i] != '\0')
-    ft_exit(data, "Error: RGB format not valid!!\n");
-    return ;
+	while (ft_is_space(str[*i]))
+		(*i)++;
+	if (str[*i] != '\0')
+		ft_exit(data, "Error: RGB format not valid!!\n");
+	return ;
 }
 
-int    ft_floor_rgb(t_data *data, const char *str)
+int	ft_floor_rgb(t_data *data, const char *str)
 {
-    int     i;
-    int     j;
-    char    *number;
+	int		i;
+	int		j;
+	char	*number;
 
-    i = 0;
-    j = 0;
-    data->floor = malloc(sizeof(int) * 3);
-    if (!data->floor)
-        return (EXIT_FAILURE);
-    while (ft_is_space(str[i]))
-        i++;
-    if (!(str[i] == 'F' && ft_is_space(str[i + 1])))
-        return (EXIT_FAILURE);
-    i++;
-    while (j < 3)
-    {
-        number = ft_read_number(str, &i);
-        if (ft_atoi(number) < 0 || ft_atoi(number) > 255)
-            return (EXIT_FAILURE);
-        data->floor[j] = ft_atoi(number);
-        free(number);
-        if (j < 2)
-            ft_check_comma(data, str, &i);
-        else if (j == 2)
-            ft_check_end(data, str, &i);
-        j++;      
-    }
-    return (EXIT_SUCCESS);
+	i = 0;
+	j = 0;
+	data->floor = malloc(sizeof(int) * 3);
+	if (!data->floor)
+		return (EXIT_FAILURE);
+	while (ft_is_space(str[i]))
+		i++;
+	if (!(str[i] == 'F' && ft_is_space(str[i + 1])))
+		return (EXIT_FAILURE);
+	i++;
+	while (j < 3)
+	{
+		number = ft_read_number(str, &i);
+		if (ft_atoi(number) < 0 || ft_atoi(number) > 255)
+			return (EXIT_FAILURE);
+		data->floor[j] = ft_atoi(number);
+		free(number);
+		if (j < 2)
+			ft_check_comma(data, str, &i);
+		else if (j == 2)
+			ft_check_end(data, str, &i);
+		j++;
+	}
+	return (EXIT_SUCCESS);
 }
 
-int    ft_ceiling_rgb(t_data *data, const char *str)
+int	ft_ceiling_rgb(t_data *data, const char *str)
 {
-    int     i;
-    int     j;
-    char    *number;
+	int		i;
+	int		j;
+	char	*number;
 
-    i = 0;
-    j = 0;
-    data->ceiling = ft_calloc(sizeof(int), 3);
-    if (!data->ceiling)
-        return (EXIT_FAILURE);
-    while (ft_is_space(str[i]))
-        i++;
-    if (!(str[i] == 'C' && ft_is_space(str[i + 1])))
-        return (EXIT_FAILURE);
-    i++;
-    while (j < 3)
-    {
-        number = ft_read_number(str, &i);
-        if (ft_atoi(number) < 0 || ft_atoi(number) > 255)
-            return (EXIT_FAILURE);
-        data->ceiling[j] = ft_atoi(number);
-        if (j < 2)
-            ft_check_comma(data, str, &i);
-        else if (j == 2)
-            ft_check_end(data, str, &i);
-        free(number);
-        j++;
-    }
-    return (EXIT_SUCCESS);
+	i = 0;
+	j = 0;
+	data->ceiling = ft_calloc(sizeof(int), 3);
+	if (!data->ceiling)
+		return (EXIT_FAILURE);
+	while (ft_is_space(str[i]))
+		i++;
+	if (!(str[i] == 'C' && ft_is_space(str[i + 1])))
+		return (EXIT_FAILURE);
+	i++;
+	while (j < 3)
+	{
+		number = ft_read_number(str, &i);
+		if (ft_atoi(number) < 0 || ft_atoi(number) > 255)
+			return (EXIT_FAILURE);
+		data->ceiling[j] = ft_atoi(number);
+		if (j < 2)
+			ft_check_comma(data, str, &i);
+		else if (j == 2)
+			ft_check_end(data, str, &i);
+		free(number);
+		j++;
+	}
+	return (EXIT_SUCCESS);
 }
